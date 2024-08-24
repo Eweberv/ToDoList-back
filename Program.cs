067fc696cb73e7600bb09a8b8c1e176d134a3da9
Victor <victor.weber@epitech.eu>
2024-08-24 03:54:56 +0200

    Kestrel config for SSL

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ using TodoList_back.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:5220", "https://0.0.0.0:443)");
+builder.WebHost.UseKestrel(opt =>
+{
+    opt.ListenAnyIP(5220);
+
+    opt.ListenAnyIP(443, listenOpt =>
+    {
+        listenOpt.UseHttps(
+            "/etc/letsencrypt/live/todolist.api.victorweber.fr/fullchain.pem", 
+            "/etc/letsencrypt/live/todolist.api.victorweber.fr/privkey.pem"
+        );
+    });
+});
+
+builder.WebHost.UseUrls("http://0.0.0.0:5220", "https://0.0.0.0:443");
 
 builder.Services.AddCors(options =>
 {
@@ -23,14 +36,14 @@ var Configuration = builder.Configuration;
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
-// Jwt config
+// Configuration JWT
 var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
 var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
 
- if (string.IsNullOrEmpty(jwtIssuer) || string.IsNullOrEmpty(jwtKey))
- {
-     throw new ArgumentNullException($"Jwt configuration is missing in appsettings.json");
- }
+if (string.IsNullOrEmpty(jwtIssuer) || string.IsNullOrEmpty(jwtKey))
+{
+    throw new ArgumentNullException($"Jwt configuration is missing in appsettings.json");
+}
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -46,7 +59,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
         };
     });
-//!Jwt config
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
